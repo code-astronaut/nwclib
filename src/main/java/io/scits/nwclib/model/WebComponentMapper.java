9936d481e3bbaa0abb26d393feaa2cdcc56d2f56
Domenik Schulz <domenik.schulz@adesso.de>
2022-12-24 01:40:10 +0100

    enhanced annotation so that the mapper is a bean and can be retrieved with di

--- src/main/java/io/scits/nwclib/model/WebComponentMapper.java
+++ src/main/java/io/scits/nwclib/model/WebComponentMapper.java
@@ -3,7 +3,7 @@ package io.scits.nwclib.model;
 import io.scits.nwclib.controller.dto.WebComponentDto;
 import org.mapstruct.Mapper;
 
-@Mapper
+@Mapper(componentModel = "spring")
 public interface WebComponentMapper {
     WebComponentDto toDto(WebComponentEntity webComponentEntity);
 }

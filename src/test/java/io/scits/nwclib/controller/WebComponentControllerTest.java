43ab485296872b7f6808791c12f996fa7161a391
Pavel Nepke <pavel.nepke@gmx.de>
2022-12-26 17:29:39 +0100

    optimized imports

--- src/test/java/io/scits/nwclib/controller/WebComponentControllerTest.java
+++ src/test/java/io/scits/nwclib/controller/WebComponentControllerTest.java
@@ -2,22 +2,13 @@ package io.scits.nwclib.controller;
 
 import com.fasterxml.jackson.databind.ObjectMapper;
 import io.scits.nwclib.controller.dto.WebComponentDto;
-import io.scits.nwclib.service.WebComponentService;
 import org.junit.jupiter.api.Test;
-import org.junit.jupiter.api.extension.ExtendWith;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
-import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
 import org.springframework.boot.test.context.SpringBootTest;
-import org.springframework.boot.test.mock.mockito.MockBean;
 import org.springframework.http.MediaType;
-import org.springframework.test.context.junit.jupiter.SpringExtension;
 import org.springframework.test.web.servlet.MockMvc;
 
-import static org.mockito.ArgumentMatchers.any;
-import static org.mockito.Mockito.times;
-import static org.mockito.Mockito.verify;
-import static org.mockito.Mockito.when;
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.post;
 import static org.springframework.test.web.servlet.result.MockMvcResultHandlers.print;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.jsonPath;

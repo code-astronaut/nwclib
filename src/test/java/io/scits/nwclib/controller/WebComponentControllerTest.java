6fcc0d30fa86d1fdfb563f11cbb6e19bb8dae077
Domenik Schulz <domenik.schulz@adesso.de>
2022-12-26 17:23:42 +0100

    fixed test

--- src/test/java/io/scits/nwclib/controller/WebComponentControllerTest.java
+++ src/test/java/io/scits/nwclib/controller/WebComponentControllerTest.java
@@ -11,20 +11,19 @@ import org.springframework.boot.test.mock.mockito.MockBean;
 import org.springframework.http.MediaType;
 import org.springframework.test.context.junit.jupiter.SpringExtension;
 import org.springframework.test.web.servlet.MockMvc;
-import org.springframework.test.web.servlet.ResultActions;
 
-import static org.hamcrest.CoreMatchers.is;
-
+import static org.mockito.ArgumentMatchers.any;
 import static org.mockito.Mockito.times;
 import static org.mockito.Mockito.verify;
-import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.*;
+import static org.mockito.Mockito.when;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.post;
 import static org.springframework.test.web.servlet.result.MockMvcResultHandlers.print;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.jsonPath;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
 
 @ExtendWith(SpringExtension.class)
 @WebMvcTest(controllers = WebComponentController.class)
-public class WebComponentControllerTest {
+class WebComponentControllerTest {
     @Autowired
     private MockMvc mockMvc;
 
@@ -44,22 +43,20 @@ public class WebComponentControllerTest {
                 .image("image")
                 .build();
 
-        ResultActions response = mockMvc.perform(post("/webcomponents")
-                .contentType(MediaType.APPLICATION_JSON)
-                .content(objectMapper.writeValueAsString(webComponent)));
+        when(webComponentService.createWebComponent(any())).thenReturn(webComponent);
 
-        response.andDo(print()).
-                andExpect(status().isCreated())
-                .andExpect(jsonPath("$.name",
-                        is(webComponent.getName())))
-                .andExpect(jsonPath("$.description",
-                        is(webComponent.getDescription())))
-                .andExpect(jsonPath("$.script",
-                        is(webComponent.getScript())))
-                .andExpect(jsonPath("$.image",
-                        is(webComponent.getImage())));
+        mockMvc.perform(post("/webcomponents")
+                        .contentType(MediaType.APPLICATION_JSON)
+                        .content(objectMapper.writeValueAsString(webComponent)))
+                .andDo(print())
+                .andExpect(status().isCreated())
+                .andExpect(jsonPath("$.name").value(webComponent.getName()))
+                .andExpect(jsonPath("$.description").value(webComponent.getDescription()))
+                .andExpect(jsonPath("$.script").value(webComponent.getScript()))
+                .andExpect(jsonPath("$.image").value(webComponent.getImage()));
 
         verify(webComponentService, times(1)).createWebComponent(webComponent);
+
 
     }
 }
